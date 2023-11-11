fc2d88deccf21fb17d54391488b791d57149b825
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-11-11 20:42:30 -0300

    Update SystemTest.java

--- GOFO Implementation/src/test/java/SystemTest.java
+++ GOFO Implementation/src/test/java/SystemTest.java
@@ -1,1 +1,23 @@
-System.out.println("coco");
+package System;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class SystemTest {
+
+    private PlayGroundSchedule playGroundSchedule;
+
+    @Before
+    public void setUp() {
+        //setup de codigo;
+    }
+ 
+    @Test
+    //teste para verificar a impressao com saldo suficiente
+    public void testIndex() {
+        assertEquals(1, playGroundSchedule.getDayIndex("sunday"));
+    }
+}
