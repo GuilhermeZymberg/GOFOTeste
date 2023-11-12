82b2a4a1d52247fb2575ff35bb6fdaf06265cb76
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-12 20:06:01 -0300

    Create AdministratorTest.java

---
+++ GOFO Implementation/src/test/java/AdministratorTest.java
@@ -0,0 +1,21 @@
+package codigo.System;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class AdministratorTest{
+  
+  private Administrator administrator;
+
+  @Before
+  public void setUp(){
+    administrator = new Administrator();
+  }
+  @Test
+  public void testGetPassword(){
+    assertEquals("123",getPassword());
+  }
+}
