cd61653c8e6cf7c9f12753918080438a5efda0b7
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-11-16 19:21:45 -0300

    Update PlayGroundScheduleTest.java

--- GOFO Implementation/src/test/java/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/PlayGroundScheduleTest.java
@@ -1,24 +1,38 @@
 package codigo.src;
 import System.PlayGroundSchedule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.Rule;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class PlayGroundScheduleTest {
 
+    @Rule
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+
     private PlayGroundSchedule playGroundSchedule;
 
     @Before
     public void setUp() {
         playGroundSchedule = new PlayGroundSchedule();
+
+        systemIn.provideLines("45");
+        playGroundSchedule.setPrice();
+        
     }
  
     @Test
     //teste para verificar a impressao com saldo suficiente
     public void testIndex() {
         assertEquals(2, playGroundSchedule.getDayIndex("monday"));
+    }
+
+    @Test
+    public void TestSetPrice(){
+        assertEquals(45,playGroundSchedule.getTimeSlotPerHour());
     }
 }
