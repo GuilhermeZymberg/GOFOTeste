179506f9880f8367c2457fff200dad5493d77422
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-16 19:37:54 -0300

    Update Administrator.java

--- GOFO Implementation/src/codigo/System/Administrator.java
+++ GOFO Implementation/src/codigo/System/Administrator.java
@@ -86,7 +86,6 @@ public class Administrator {
             System.out.println("Do you want to Approve this playground? (yes or no)");
             String getAns = input.nextLine();
             if (getAns.equalsIgnoreCase("yes")) {
-                System.out.println("yeeeeeeeeeeeeeeeeee");
                 Approved.add(Requested.get(i));
                 Requested.remove(Requested.get(i));
                 i--;
