3859757a42c817308aeb5f710f1285059c518b52
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-19 19:32:33 -0300

    Update UserRegistrationTest.java

--- GOFO Implementation/src/test/java/UserRegistrationTest.java
+++ GOFO Implementation/src/test/java/UserRegistrationTest.java
@@ -2,6 +2,7 @@ package codigo.src;
 import System.userProfile;
 import UI.SystemUI;
 import System.Player;
+import System.PlaygroundOwner;
 import java.io.*;
 import java.util.ArrayList;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
@@ -20,6 +21,7 @@ public class UserRegistrationTest{
   private userProfile user2;
   private SystemUI s;
   private Player p;
+  private PlaygroundOwner pg;
   static ArrayList<Player> players = new ArrayList<Player>();
   
   @Rule
@@ -27,6 +29,7 @@ public class UserRegistrationTest{
   @Before
   public void setUp(){
     p = new Player();
+    pg =  new PlaygroundOwner();
     s = new SystemUI();
     user1 = new userProfile();
     user2 = new userProfile();
@@ -37,7 +40,8 @@ public class UserRegistrationTest{
     System.out.println("--Main Flow--");
     systemIn.provideLines("gui","z", "7","1234", "gui@gm.com","11", "sp","player","50"); //balance, fname, lname, ID, pass, email, phone, loc, balance
     s.register();
-    p.setBalance(50);
+    systemIn.provideLines("50");
+    p.setBalance();
     p.setFName("gui");
     p.setLName("z");
     p.setPassword("1234");
@@ -55,15 +59,16 @@ public class UserRegistrationTest{
     System.out.println("--Exception Flow--");
     systemIn.provideLines("2","clara","r", "5","5678", "clahrck@gm.com","52", "rs","playground owner","50"); // invalid, fname, lname, ID, pass, email, phone, loc, balance
     s.register();
-    p.setBalance(50);
-    p.setFName("clara");
-    p.setLName("r");
-    p.setPassword("5678");
-    p.setID(5);
-    p.setRule("playground owner");
-    p.setPhone(52);
-    p.setEmail("clahrck@gm.com");
-    p.setLocation("rs");
+    systemIn.provideLines("50");
+    pg.setBalance();
+    pg.setFName("clara");
+    pg.setLName("r");
+    pg.setPassword("5678");
+    pg.setID(5);
+    pg.setRule("playground owner");
+    pg.setPhone(52);
+    pg.setEmail("clahrck@gm.com");
+    pg.setLocation("rs");
     assertEquals(p,s.getThePlayers().get(0));
     System.out.println("----END OF REGISTRATION TEST----");
   }
