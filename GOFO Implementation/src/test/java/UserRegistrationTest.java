c5c6efb73c04e684c7c2fe68f264dfabecd847a3
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-19 18:00:48 -0300

    Update UserRegistrationTest.java

--- GOFO Implementation/src/test/java/UserRegistrationTest.java
+++ GOFO Implementation/src/test/java/UserRegistrationTest.java
@@ -3,6 +3,7 @@ import System.userProfile;
 import UI.SystemUI;
 import System.Player;
 import java.io.*;
+import java.util.ArrayList;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import java.io.IOException;
@@ -18,21 +19,34 @@ public class UserRegistrationTest{
   private Player p;
   private userProfile user;
   private SystemUI s;
+  static ArrayList<Player> players = new ArrayList<Player>();
   @Rule
   public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
   @Before
   public void setUp(){
     p = new Player();
     user = new userProfile();
-    s = new SystemUI();
   }
   @Test
   public void editPlayerInfoTest(){
-    systemIn.provideLines("1","gui");
+    System.out.println("----REGISTRATION TEST----");
+    systemIn.provideLines("1","gui"); //fname
     p.editPlayerinfo();
-    systemIn.provideLines("2","ze");
+    systemIn.provideLines("2","ze"); //lname
     p.editPlayerinfo();
-    System.out.println(p.getFullName());
     assertEquals("gui ze", p.getFullName());
+    systemIn.provideLines("3","7"); //ID
+    p.editPlayerinfo();
+    systemIn.provideLines("4","gui@gm.com"); //email
+    p.editPlayerinfo();
+    systemIn.provideLines("7","1234"); //password
+    p.editPlayerinfo();
+    systemIn.provideLines("5","(11) 99999-9999"); //phone
+    p.editPlayerinfo();
+    systemIn.provideLines("6","7"); //location
+    p.editPlayerinfo();
+    players.add(p);
+    p.obj.thePlayers.add(p);
+    assertEquals(players,p.obj.thePlayer);
   }
 }
