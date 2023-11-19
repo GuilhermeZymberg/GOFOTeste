6297fa06ad49e9587c5975643df58deb5ebfbf83
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-19 18:49:37 -0300

    Update SystemUI.java

--- GOFO Implementation/src/codigo/UI/SystemUI.java
+++ GOFO Implementation/src/codigo/UI/SystemUI.java
@@ -18,7 +18,7 @@ import System.PlaygroundOwner;
  * @author Mohamed_El_Laithy
  */
 public class SystemUI {
-
+    Scanner input = new Scanner(System.in);
     static String currentUser = "A";//Current logged in user
     static int userBalance = 0;//Current user balance
     static int userIndex = 0;//Current user Index
@@ -33,7 +33,7 @@ public class SystemUI {
      * @return phrase
      */
     public static String isString(String phrase) {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         if (phrase != null && !phrase.equals("") && phrase.matches("^[a-zA-Z]*$")) {
             return phrase;
         } else {
@@ -49,7 +49,7 @@ public class SystemUI {
      * @return integer
      */
     public static int stringToInt(String phrase) {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         int integer = 0;
         if (phrase == null) {
             System.out.println("please enter value");
@@ -72,7 +72,7 @@ public class SystemUI {
      * @return phrase
      */
     public static String isDigit(String phrase) {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         int integer = 0;
         if (phrase == null) {
             System.out.println("Please enter value");
@@ -93,7 +93,7 @@ public class SystemUI {
      * Account Menu function it contain Login and Register and Exit From System
      */
     public static void accountMenu() {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         System.out.println("Pick a choice");
         System.out.println("1-Log in");
         System.out.println("2-Register");
@@ -135,7 +135,7 @@ public class SystemUI {
      * get validation code for the email
      */
     public static void getValidationCode() {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         System.out.println("Enter the validation code that was sent to your email.");
         String getCode = input.nextLine();
         System.out.println("Successfully verified!");
@@ -148,7 +148,7 @@ public class SystemUI {
      * @return email
      */
     public static String isEmail(String email) {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         String emailReg = "^[a-zA-Z0-9_+&-]+(?:\\." + "[a-zA-Z0-9_+&-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
         Pattern emaill = Pattern.compile(emailReg);
         if (email == null || email.equals("")) {
@@ -169,7 +169,7 @@ public class SystemUI {
      * registering account for a user
      */
     public static void register() {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         String fName, lName, password, temp, email, role;
         int ID, phone;
         System.out.println("Enter the info to add new user");
@@ -236,7 +236,7 @@ public class SystemUI {
      * logging into the user's account
      */
     public static void Login() {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         String email;
         boolean correct = false;
         System.out.println("Log in");
@@ -284,7 +284,7 @@ public class SystemUI {
      * Complaining about something to the administrators
      */
     public static void complaintForm() {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         String personEmail, personType, complaint = null, comp;
         boolean bool = false, inputChecker = true;
         while (inputChecker) {
