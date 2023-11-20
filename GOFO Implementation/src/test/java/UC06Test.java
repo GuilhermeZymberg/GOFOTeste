afba4780213d7d99bfa19d1bafdf3b43952a5c5e
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-11-20 19:20:41 -0300

    Create UC06Test.java

---
+++ GOFO Implementation/src/test/java/UC06Test.java
@@ -0,0 +1,39 @@
+package codigo.src;
+import System.Administrator;
+import System.Playground;
+import UI.PlayerUI;
+import java.io.*;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class UC06Test {
+
+  private Playground playGround;
+  private Administrator admin;
+  private PlayerUI player;
+
+  systemIn.provideLines("45");
+  
+  @Rule
+  public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+
+  @Before
+  public void setUp() {
+    admin = new Administrator();
+    player = new PlayerUI();
+    playGround = new Playground("Playground1", "Pedro", "SBC", 2, 20, "available", playGroundSchedule);
+  }
+
+  @Test
+  public void Teste(){
+    admin.searchByName("Playground1");
+  }
+}
