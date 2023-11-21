359bc93ea490f5ee7dfda0fba596408075dd3bd1
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-21 02:17:17 -0300

    Update BookingTest.java

--- GOFO Implementation/src/test/java/BookingTest.java
+++ GOFO Implementation/src/test/java/BookingTest.java
@@ -1,11 +1,14 @@
 package codigo.src;
 import System.Administrator;
 import System.Playground;
+import System.PlaygroundOwner;
 import System.Player;
+import System.eWallet;
 import UI.PlayerUI;
 import java.util.ArrayList;
 import java.io.*;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import java.io.IOException;
 
@@ -26,7 +29,9 @@ public class BookingTest{
   private PlayerUI pu;
   private ArrayList<Player> ps;
   @Rule
-  public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+  @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
   @Before
   public void setUp(){
     p = new Playground();
@@ -35,6 +40,9 @@ public class BookingTest{
     p1 = new Player();
     p2 = new Player();
     p3 = new Player();
+    po = new PlaygroundOwner();
+    //po setup
+    e = new eWallet();
     System.out.println("----UC04 - BOOKING TEST----");
     System.out.println("--Main Flow--");
     //playground setup
@@ -64,36 +72,48 @@ public class BookingTest{
     p1.setPhone(24);
     p1.setEmail("gege@gml.com");
     p1.setLocation("jj");
-    systemIn.provideLines("50");
     
     //p2 setup
     systemIn.provideLines("50");
     p2.setBalance();
-    p2.setFName("gege");
-    p2.setLName("akutami");
-    p2.setPassword("abcd");
-    p2.setID(10);
+    p2.setFName("mo");
+    p2.setLName("mo");
+    p2.setPassword("din");
+    p2.setID(5);
     p2.setRule("player");
-    p2.setPhone(24);
-    p2.setEmail("gege@gml.com");
-    p2.setLocation("jj");
+    p2.setPhone(99);
+    p2.setEmail("momo@gml.com");
+    p2.setLocation("mt");
     //p3 setup
     systemIn.provideLines("50");
     p3.setBalance();
-    p3.setFName("gege");
-    p3.setLName("akutami");
-    p3.setPassword("abcd");
-    p3.setID(10);
+    p3.setFName("titi");
+    p3.setLName("toto");
+    p3.setPassword("xddd");
+    p3.setID(8);
     p3.setRule("player");
-    p3.setPhone(24);
-    p3.setEmail("gege@gml.com");
-    p3.setLocation("jj");
+    p3.setPhone(56);
+    p3.setEmail("tito@gml.com");
+    p3.setLocation("tt");
 
     p1.createTeam(p2);
     p1.createTeam(p3);
     ps.add(p1);
     ps.add(p2);
     ps.add(p3);
+
+    systemIn.provideLines("50");
+    e.setBalance();
+    po.setBalance(e);
+    po.setFName("mario");
+    po.setLName("kart");
+    po.setPassword("yoy");
+    po.setID(33);
+    po.setRule("player");
+    po.setPhone(1);
+    po.setEmail("murilo@gml.com");
+    po.setLocation("mk");
+    po.addPlayground(p);
     
   }
   @Test
@@ -104,6 +124,7 @@ public class BookingTest{
     for(int i = 1; i < ps.size(); i++){
       ps.get(i).addInbox("Invited for "+ p.getName() + " from 3 to 5 PM at sunday");
       ps.get(i).viewInbox(); //verificar se impressão foi que estava vazia ou não
+      assertEquals("Invited for "+ p.getName() + " from 3 to 5 PM at sunday", systemOutRule.getLog());
     }
     
   }
@@ -112,8 +133,19 @@ public class BookingTest{
     System.out.println("--Exception Flow--");
     systemIn.provideLines("not available");
     p.setStatus();
-    systemIn.provideLines("3","2","sunday");
+    systemIn.provideLines("3","2","sunday"); //as 3 - 5, sunday
     assertEquals(10,administrator.bookByName("Teste",p1.getFullName(),p1.getBalance())); //expected error, should ask for new status but will return 5
     System.out.println("----END OF UC04 - BOOKING TEST----");
+  }
+  @Test
+  public void cancelBookingTest(){
+    System.out.println("----UC03 - CANCEL BOOKING TEST----");
+    System.out.println("--Main Flow--");
+    p1.CancelBooking("Teste","3");
+    for(i = 3; i < 5; i++){
+      p.cancelBooking("sunday",Integer.toString(i));
+    }
+    p.setStatus("available");
+    p.displayFreeSlots();
   }
 }
