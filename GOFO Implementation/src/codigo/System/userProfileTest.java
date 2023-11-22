24181dffe30e982731a4015f0c47cf66de1db8a7
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-22 02:56:54 -0300

    Create userProfileTest.java

---
+++ GOFO Implementation/src/codigo/System/userProfileTest.java
@@ -0,0 +1,31 @@
+package codigo.src;
+import System.userProfile;
+import java.io.*;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class userProfileTest{
+  
+  private userProfile u;
+  
+  @Rule
+  public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+  @Before
+  public void setUp(){
+    u = new userProfile();
+  }
+  @Test
+  public void getFullNameTest(){
+    u.setFName("vovó");
+    u.setLName("juju");
+    assertEquals("vovó juju",u.getFullName());
+  }
+}
