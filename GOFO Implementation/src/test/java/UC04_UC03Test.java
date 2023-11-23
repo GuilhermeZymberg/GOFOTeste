0c4bbfc5f42b5de87c8eaebc302f24bbe2b859f2
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-22 21:37:17 -0300

    Update UC04_UC03Test.java

--- GOFO Implementation/src/test/java/UC04_UC03Test.java
+++ GOFO Implementation/src/test/java/UC04_UC03Test.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertNotEquals;
 
-public class BookingTest{
+public class UC04_UC03Test{
   private Administrator administrator;
   private Player p1;
   private Player p2;
