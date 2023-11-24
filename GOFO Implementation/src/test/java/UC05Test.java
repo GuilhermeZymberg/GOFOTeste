9ac5b1c65e25df2e9b8132bf8b574a81d1d8179c
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 14:17:23 -0300

    Update UC05Test.java

--- GOFO Implementation/src/test/java/UC05Test.java
+++ GOFO Implementation/src/test/java/UC05Test.java
@@ -1,11 +1,7 @@
 package codigo.src;
-import System.PlayGroundSchedule;
-import System.Administrator;
-import System.Playground;
-import System.Player;
-import UI.PlayerUI;
+import UI.SystemUI;
 import java.io.*;
-import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -29,29 +25,19 @@ public class UC05Test {
   private ArrayList<Player> Aplayer = new ArrayList<Player>();
   
   @Rule
-  public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
   @Rule
   public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
 
   @Before
   public void setUp() {
-    player = new Player();
-    player.setEmail("pedro@gmail.com");
-    playGroundSchedule = new PlayGroundSchedule("available", 2, 1, 3);
-    playGround = new Playground("Playground1", "Pedro", "SBC", 2, 20, "available", playGroundSchedule);
-    admin = new Administrator();
-    admin.playgroundRequests(playGround);
-    systemIn.provideLines("yes");
-    admin.approvePlayground();
-
-    pui = new PlayerUI();
+    s = new SystemUI();
   }
 
   @Test
   public void TesteUC05(){
-    Scanner input = new Scanner(System.in);
-    System.out.println("UC05 =============");
-    systemIn.provideLines("3", "1", "SBC", "12", "3");
-    pui.playerMenu(input, 0, Aplayer, "Pedro", 45);
+    exit.expectSystemExitWithStatus(0);
+     systemIn.provideLines("2","gui","z", "7","1234", "gui@gm.com","11", "sp","playground owner","50","bolinha","1","gui@gm.com","1234","1","cetobarrero","gg","4","available","1","4","10","8","1","admin@gmail.com","123","1","yes","6","2","gu","z", "7","1234", "gu@gm.com","11", "sp","player","1","bolinha","1","gu@gm.com","1234","3","2","cetobarrero","1","2","saturday","12","3");
+    s.accountMenu();
   }
 }
