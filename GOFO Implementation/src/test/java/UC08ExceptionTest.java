ff7f636f02a087543eab3f39a65659888ac04512
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 00:15:07 -0300

    Create UC08ExceptionTest.java

---
+++ GOFO Implementation/src/test/java/UC08ExceptionTest.java
@@ -0,0 +1,46 @@
+package codigo.src;
+import System.PlayGroundSchedule;
+import System.Administrator;
+import System.Playground;
+import System.Player;
+import UI.PlayerUI;
+import java.io.*;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class UC08Test {
+  private PlayGroundSchedule playGroundSchedule;
+  private Playground playGround;
+  private Administrator admin;
+  private Player player, player2;
+  private PlayerUI pui;
+  private ArrayList<Player> Aplayer = new ArrayList<Player>();
+  
+  @Rule
+  public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+  @Rule
+  public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+   
+  @Before
+  public void setUp() {
+    player = new Player();
+    player2 = new Player();
+    Aplayer.add(player);
+    Aplayer.add(player2);
+    player.setEmail("pedro@gmail.com");
+    player2.setEmail("pedro2@gmail.com");
+    playGroundSchedule = new PlayGroundSchedule("available", 2, 1, 3);
+    playGround = new Playground("Playground1", "Pedro", "SBC", 2, 20, "available", playGroundSchedule);
+  }
+}
