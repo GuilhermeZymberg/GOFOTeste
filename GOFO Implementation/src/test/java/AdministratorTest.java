7b6db51ea9fff1cd09b02ce2fc026fdec730c5a9
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 17:21:43 -0300

    Update AdministratorTest.java

--- GOFO Implementation/src/test/java/AdministratorTest.java
+++ GOFO Implementation/src/test/java/AdministratorTest.java
@@ -76,7 +76,7 @@ public class AdministratorTest{
   @Test
   public void testBookByLocations(){
     System.out.println("----TESTE BOOK BY LOCATION----");
-    systemIn.provideLines("2","1","monday"); //hora: 1h, duração de 1h, no dia 12
+    systemIn.provideLines("1","1","monday"); //hora: 1h, duração de 1h, no dia 12
     int res = administrator.bookByLocation("sp","james",51); 
     assertEquals(50,res);
     System.out.println("----FIM BOOK BY LOCATION----");
