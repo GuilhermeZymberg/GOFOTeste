9a08082c412da1d0546b5ad0322eaeca26fc387d
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 14:18:07 -0300

    Create UC05ExceptionTest.java

---
+++ GOFO Implementation/src/test/java/UC05ExceptionTest.java
@@ -0,0 +1,43 @@
+package codigo.src;
+import UI.SystemUI;
+import java.io.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class UC05ExceptionTest {
+
+  private PlayGroundSchedule playGroundSchedule;
+  private Playground playGround;
+  private Administrator admin;
+  private Player player;
+  private PlayerUI pui;
+  private ArrayList<Player> Aplayer = new ArrayList<Player>();
+  
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+  @Rule
+  public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+
+  @Before
+  public void setUp() {
+    s = new SystemUI();
+  }
+
+  @Test
+  public void TesteUC05(){
+    exit.expectSystemExitWithStatus(0);
+     systemIn.provideLines("2","gui","z", "7","1234", "gui@gm.com","11", "sp","playground owner","50","bolinha","1","gui@gm.com","1234","1","cetobarrero","gg","4","available","1","4","10","8","1","admin@gmail.com","123","1","yes","6","2","gu","z", "7","1234", "gu@gm.com","11", "sp","player","1","bolinha","1","gu@gm.com","1234","3","2","cetobarrero","1","2","saturday","12","3");
+    s.accountMenu();
+  }
+}
