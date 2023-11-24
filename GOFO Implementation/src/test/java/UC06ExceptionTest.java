e0cd425846baa3e5b5981b789ccd95831b567010
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-11-24 00:20:50 -0300

    Update UC06ExceptionTest.java

--- GOFO Implementation/src/test/java/UC06ExceptionTest.java
+++ GOFO Implementation/src/test/java/UC06ExceptionTest.java
@@ -46,4 +46,12 @@ public class UC06Test {
 
     pui = new PlayerUI();
   }
+
+  @Test
+  public void TesteUC06_Excep(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("UC06 =============");
+    systemIn.provideLines("11", "SaBC", "12", "3");
+    pui.playerMenu(input, 0, Aplayer, "Pedro", 45);
+  }
 }
