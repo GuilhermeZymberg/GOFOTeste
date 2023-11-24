eeecf783dadfc8bc5e07f137ed11db2f33d34eac
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 00:52:00 -0300

    Update UC01Test.java

--- GOFO Implementation/src/test/java/UC01Test.java
+++ GOFO Implementation/src/test/java/UC01Test.java
@@ -36,14 +36,14 @@ public class UC01Test{
     s = new SystemUI();
     user1 = new userProfile();
     user2 = new userProfile();
-  }/*
+  }
   @Test
   public void registerTest(){
     System.out.println("----UC01 - REGISTRATION TEST----");
     System.out.println("--Main Flow--");
-    systemIn.provideLines("gui","z", "7","1234", "gui@gm.com","11", "sp","player","50"); //balance, fname, lname, ID, pass, email, phone, loc, balance
-    s.register();
-    systemIn.provideLines("50");
+    systemIn.provideLines("2","gui","z", "7","1234", "gui@gm.com","11", "sp","player","50","bolinha","3"); //option, fname, lname, ID, pass, email, phone, loc, balance, validation code, option
+    s.accountMenu();
+    /*systemIn.provideLines("50");
     p.setBalance();
     p.setFName("gui");
     p.setLName("z");
@@ -59,30 +59,6 @@ public class UC01Test{
     assertEquals(p.getID(),s.getThePlayers().get(0).getID());
     assertEquals(p.getRule(),s.getThePlayers().get(0).getRule());
     assertEquals(p.getPhone(),s.getThePlayers().get(0).getPhone());
-    assertEquals(p.getEmail(),s.getThePlayers().get(0).getEmail());
+    assertEquals(p.getEmail(),s.getThePlayers().get(0).getEmail());*/
   }
-  public void registerExceptionTest(){
-    System.out.println("--Exception Flow--");
-    systemIn.provideLines("2","clara","r", "5","5678", "clahrck@gm.com","52", "rs","playground owner","50"); // invalid, fname, lname, ID, pass, email, phone, loc, balance
-    s.register();
-    systemIn.provideLines("50");
-    e.setBalance();
-    pg.setBalance(e);
-    pg.setFName("clara");
-    pg.setLName("r");
-    pg.setPassword("5678");
-    pg.setID(5);
-    pg.setRule("playground owner");
-    pg.setPhone(52);
-    pg.setEmail("clahrck@gm.com");
-    pg.setLocation("rs");
-    assertEquals(pg.getMyBalance(),s.getTheOwners().get(0).getMyBalance());
-    assertEquals(pg.getFullName(),s.getTheOwners().get(0).getFullName());
-    assertEquals(pg.getPassword(),s.getTheOwners().get(0).getPassword());
-    assertEquals(pg.getID(),s.getTheOwners().get(0).getID());
-    assertEquals(pg.getRule(),s.getTheOwners().get(0).getRule());
-    assertEquals(pg.getPhone(),s.getTheOwners().get(0).getPhone());
-    assertEquals(pg.getEmail(),s.getTheOwners().get(0).getEmail());
-    System.out.println("----UC01 - END OF REGISTRATION TEST----");
-  }*/
 }
