12c200fce2a5ad4879c48203c44803d5ffd11e99
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-11-24 13:02:59 -0300

    Update UC08Test.java

--- GOFO Implementation/src/test/java/UC08Test.java
+++ GOFO Implementation/src/test/java/UC08Test.java
@@ -50,7 +50,7 @@ public class UC08Test {
     Scanner input = new Scanner(System.in);
     System.out.println("UC08 - Case 1");
     pui = new PlayerUI();
-    systemIn.provideLines("4", "1", "pedro2@gmail.com", "12");
+    systemIn.provideLines("4", "1", "pedro2@gmail.com", "12","3");
     pui.playerMenu(input, 0, Aplayer, "Pedro", 45);
   }
 }
